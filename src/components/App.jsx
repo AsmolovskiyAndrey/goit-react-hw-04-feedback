f5cf9ea0d587c6bf29f6990d50b8d957558c0612
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2023-01-04 23:26:16 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { FeedbackOptions } from './Feedback/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { NoAnswer } from './NoAnswer/NoAnswer';
